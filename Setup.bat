870af66c2ff8f2838f6222a337fa759d476de0a9
Matthew <matt@vevox.io>
2019-08-30 21:22:10 -0700

    create setup scripts, start on source

---
+++ Setup.bat
@@ -0,0 +1,1 @@
+powershell "Start-Process powershell -Verb runAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %~dp0\Setup.ps1'"
--- Source/PipLib.cs
+++ Source/PipLib.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Source
+namespace PipLib
 {
-    public class Class1
+    public class PipLib
     {
     }
 }
