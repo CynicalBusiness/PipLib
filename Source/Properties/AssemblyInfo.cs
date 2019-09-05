f69609aefbd41f3943830f38adf184087f60b724
Matthew <matt@vevox.io>
2019-09-05 10:50:04 -0700

    minor build config changes

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.19363.0")]
-[assembly: AssemblyFileVersion("1.0.19363.3")]
+[assembly: AssemblyVersion("1.0.19364.0")]
+[assembly: AssemblyFileVersion("1.0.19364.6")]
