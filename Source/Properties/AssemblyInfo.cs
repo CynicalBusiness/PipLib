642c364445fa94409e90324c44be41bafb7d1f0c
CynicalBusiness <matt@vevox.io>
2019-09-24 20:24:51 -0700

    copy licenses with name instead

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.19392.0")]
-[assembly: AssemblyFileVersion("1.0.19392.56")]
+[assembly: AssemblyFileVersion("1.0.19392.58")]
