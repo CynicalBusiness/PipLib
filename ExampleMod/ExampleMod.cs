dc96c54913a76fe34ecbe3787a85d642143553aa
CynicalBusiness <matt@vevox.io>
2019-10-02 23:07:17 -0700

    example mod

--- ExampleMod/ExampleMod.cs
+++ ExampleMod/ExampleMod.cs
@@ -1,39 +1,55 @@
-﻿using PipLib.Mod;
-using PipLib.World;
+using PipLib.Mod;
+using PipLib.Tech;
 
 namespace ExampleMod
 {
+
+    // to create a mod, simply extend PipMod
     public class ExampleMod : PipMod
     {
+        // this is the public name of your mod, and is required
+        public override string Name => "ExampleMod";
 
-        public const string NAME = "Example";
-
-        public ExampleMod() : base(NAME) { }
+        // optionally, you can provide a prefix for logging and namespacing
+        public override string Prefix => "Example";
 
         public override void Load()
         {
-            // load an asset bundle into the AssetLoader
-            LoadAssetBundle("resources.assets");
+            // occurs after this mod has been loaded (but not necessarily all)
+        }
 
-            // let's make some new elements
-            // in this case, tin and it's ore, casserite, as well as bronze
-            var tin = new PipElement(this, "Tin") { name = "Tin" }
-                .AddSolid() // Add a solid state for our element. This creates a material and KAnim.
-                            // .AddLiquid()
-                            // .AddGas()
-                .AddBuildingOverheatModifier(-20f) // set a flat -20C overheat modifier, like lead
-                .AddBuildingDecorModifier(0.1f); // and let's give it a +10% decor bonus
-            var tinOre = new PipElement(this, "TinOre") { name = "Casserite", desc = $"A raw ore that can be refined into {STRINGS.UI.FormatAsLink("Tin", tin.SimId(Element.State.Solid))}." }
-                .AddSolid() // same deal here
-                .AddBuildingOverheatModifier(-20f);
-            var bronze = new PipElement(this, "Bronze") { name = "Bronze", desc = $"A strong alloy formed from {STRINGS.UI.FormatAsLink("Tin", tin.SimId(Element.State.Solid))} and {STRINGS.UI.FormatAsLink("Copper", nameof(STRINGS.ELEMENTS.COPPER))}." }
-                .AddSolid()
-                .AddBuildingOverheatModifier(75f)
-                .AddBuildingDecorModifier(0.2f);
-            // we'll also want to define these elements in an `elements.yaml` to set things like material properties and tags.
+        public override void PostLoad()
+        {
+            // occurs after all mods have been loaded
+        }
 
-            // register our new elements with the game
-            AddElements(new PipElement[] { tin, tinOre });
+        public override void PreInitialize()
+        {
+            // before database initialization
+        }
+
+        public override void Initialize()
+        {
+            // after database initialization
+
+            // === CUSTOM TECH
+            // using PipLib.Tech
+            // creating tech happens after the database has been initialized
+            // the research tree will be updated automatically
+            var tech = TechTree.CreateTech("ExampleTech");
+            // add the "farming" tech as a requirement to ours
+            // this also assigns the tier to be one greater than farming's tier
+            // this can be overridden with `TechTree.SetTier()`
+            TechTree.AddRequirement(tech, TechTree.GetTech("FarmingTech"));
+            // a tech needs to have "unlockedItems" or an error will appear
+            // you can either register a build to this tech now, or add a non-building TechItem
+            tech.unlockedItems.Add(Db.Get().TechItems.jetSuit);
+            // note that `TechItems.gammaResearchPoint` causes a crash when used
+        }
+
+        public override void PostInitialize()
+        {
+            // after database initialization, all mods initialize, and most patches to Db.Initialize()
         }
     }
 }
